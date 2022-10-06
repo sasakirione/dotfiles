d9aa0d6ff500430b49e58f124cf1f71a739946fb
sasakirione <sasakirione@gmail.com>
2022-10-07 04:50:47 +0900

    Add: VScodeインストール用のシェル追加

---
+++ ubuntu_installer/code.sh
@@ -0,0 +1,6 @@
+apt install -y curl apt-transport-https
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt update
+apt install -y code
