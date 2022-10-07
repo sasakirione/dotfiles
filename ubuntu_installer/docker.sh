ba3bb564e2e7c55747de1d1bfd4a695859117a98
sasakirione <sasakirione@gmail.com>
2022-10-07 23:55:10 +0900

    Update: Dockerインスココードの改修

--- ubuntu_installer/docker.sh
+++ ubuntu_installer/docker.sh
@@ -1,16 +1,7 @@
 sudo apt update
-sudo apt install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 sudo apt update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt install docker-ce
